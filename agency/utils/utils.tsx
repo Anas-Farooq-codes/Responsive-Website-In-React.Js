27f55a69d825dd832cc594a28e14820d9a59a230
Anas Farooq <anasfarooq2006@gmail.com>
2024-12-01 00:25:30 +0500

    Update utils.tsx

--- agency/utils/utils.tsx
+++ agency/utils/utils.tsx
@@ -223,8 +223,6 @@ export const faqs = [
 
 type Link = { name: string; url?: string; email?: string; phone?: string };
 
-
-
 export const footerNavLinks: { title: string; links: Link[] }[] = [
   {
     title: "About Us",
